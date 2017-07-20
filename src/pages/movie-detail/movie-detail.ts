86525c21f4b40d7968c7d46de682e2f7208b70b7
mhf <2639482456@qq.com>
2017-07-20 08:34:11 +0800

    页面跳转

---
+++ src/pages/movie-detail/movie-detail.ts
@@ -0,0 +1,26 @@
+import { Component } from '@angular/core';
+import { NavController, NavParams } from 'ionic-angular';
+
+/**
+ * Generated class for the MovieDetailPage page.
+ *
+ * See http://ionicframework.com/docs/components/#navigation for more info
+ * on Ionic pages and navigation.
+ */
+
+@Component({
+  selector: 'page-movie-detail',
+  templateUrl: 'movie-detail.html',
+})
+export class MovieDetailPage {
+  id:number;
+
+  constructor(public navCtrl: NavController, public navParams: NavParams) {
+    this.id = navParams.get('id');
+  }
+
+  ionViewDidLoad() {
+    console.log('ionViewDidLoad MovieDetailPage');
+  }
+
+}
