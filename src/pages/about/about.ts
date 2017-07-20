13cd1374b245a1d522ad6ede997f68f94ab1ee90
mhf <2639482456@qq.com>
2017-07-20 20:17:41 +0800

    看片页链接数据库动态化

--- src/pages/about/about.ts
+++ src/pages/about/about.ts
@@ -1,20 +1,33 @@
 import { Component } from '@angular/core';
 import { NavController } from 'ionic-angular';
 import {MovieDetailPage} from "../movie-detail/movie-detail";
+import 'rxjs/add/operator/map';
+import {Http} from "@angular/http";
 
 @Component({
   selector: 'page-about',
   templateUrl: 'about.html'
 })
 export class AboutPage {
-  pet: string='puppies';
-  puppies:string='onShow';
-  constructor(public navCtrl: NavController) {
+  pet: string='kittens';
+  icon:string='onShow';
+  imgs=[];
+  // title=[];
+  constructor(public navCtrl: NavController,public http:Http) {
 
   }
+  get(){
+    this.http.get('http://localhost:3000/d/w').toPromise().then((res)=>{
+      // console.log(res);
+      console.log(res.json().data);
+      this.imgs = res.json().data;
 
-  detail(id){
-    this.navCtrl.push(MovieDetailPage,{id})
+      console.log(this.imgs);
+    })
+  }
+  detail(a){
+    console.log(a);
+    this.navCtrl.push(MovieDetailPage,{a})
   }
 
 }
